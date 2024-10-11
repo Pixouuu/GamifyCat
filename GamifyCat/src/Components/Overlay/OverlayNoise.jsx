c7fcd95ffd1ab3a130923368989dcd0c4f729dcc
Pixouuu <theo35111@gmail.com>
2024-10-11 12:12:29 +0200

    Overlay Update

--- GamifyCat/src/Components/Overlay/OverlayNoise.jsx
+++ GamifyCat/src/Components/Overlay/OverlayNoise.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function OverlayNoise() {
   return (
-    <div className='blur-[0.5px] bg-[url("../../src/assets/noise.gif")] bg-repeat opacity-[0.05] h-full w-full z-[5] absolute pointer-events-none '></div>
+    <div className=' blur-[0.5px] bg-[url("/public/noise.gif")] bg-repeat opacity-[0.05] h-full w-full z-[999] absolute pointer-events-none '></div>
   )
 }
 
